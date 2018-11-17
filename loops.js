ed3f2b11f4e792fb0fc37b3d6b48d90ed8e72e6e
Fei Qi <feiqi9047@gmail.com>
2018-11-17 19:57:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
   for (let i = 0; i<25; i++) {
   if (i===1){
-    console.log("I am " + i + " strange loop.");
+    array.push("I am " + i + " strange loop.");
   }else{
-    console.log("I am " + i + " strange loops.");
+    aray.push("I am " + i + " strange loops.");
   }
 }
 return array
